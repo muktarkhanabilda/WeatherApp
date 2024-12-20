8898585d0858e291d59806fc42e3f9c9d252b3e1
muktarkhanabilda <117581921+muktarkhanabilda@users.noreply.github.com>
2024-12-21 04:25:16 +0600

    m

--- app/src/main/java/com/example/weatherapp_final/adapters/WeatherAdapter.kt
+++ app/src/main/java/com/example/weatherapp_final/adapters/WeatherAdapter.kt
@@ -1,18 +1,47 @@
 package com.example.weatherapp_final.adapters
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.weatherapp_final.R
 import com.example.weatherapp_final.databinding.ListItemBinding
 
-class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder > {
 
-    class Holder(view: View) : RecyclerView.ViewHolder(view) {
+//class WeatherAdapter : ListAdapter<WeatherModel, //WeatherssAdapter.Holder>() {
+class WeatherAdapter : ListAdapter<WeatherModel, WeatherAdapter.Holder>(Comparator()) {
+    class Holder(view: View) : RecyclerView.ViewHolder(view){
+
         val binding = ListItemBinding.bind(view)
-        fun bind(item: WeatherModel) = with(binding) {
-            tvDate.text = item.time
-            tvCondition.text = item.condition
-            tvTemp.text = item.currentTemp.ifEmpty { "${item.maxTemp}°C / ${item.minTemp}°C" }
+
+        fun bind(item: WeatherModel) = with(binding){
+            tvDate2.text = item.time
+            tvCondition2.text = item.condition
+            tvTemp.text = item.currentTemp
+            }
         }
+
+    class Comparator : DiffUtil.ItemCallback<WeatherModel>(){
+        override fun areItemsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: WeatherModel, newItem: WeatherModel): Boolean {
+            return oldItem == newItem
+        }
+
+
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Holder(view)
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        return holder.bind(getItem(position))
+    }
+
 }
--- app/src/main/java/com/example/weatherapp_final/fragments/HoursFragment.kt
+++ app/src/main/java/com/example/weatherapp_final/fragments/HoursFragment.kt
@@ -5,16 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.weatherapp_final.R
+import com.example.weatherapp_final.adapters.WeatherAdapter
+import com.example.weatherapp_final.adapters.WeatherModel
+import com.example.weatherapp_final.databinding.FragmentHoursBinding
 
 class HoursFragment : Fragment() {
+
+    private lateinit var binding: FragmentHoursBinding
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hours, container, false)
+        binding = FragmentHoursBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRcView()
+    }
+
+    private fun initRcView() = with(binding){
+        rcView.layoutManager = LinearLayoutManager(activity)
+        adapter = WeatherAdapter()
+        rcView.adapter = adapter
+        val list = listOf(
+            WeatherModel(
+                "","12:00",
+                "Sunny","25ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","13:00",
+                "Sunny","25ºC",
+                "", "", "",""),
+            WeatherModel(
+                "","14:00",
+                "Sunny","35ºC",
+                "", "", "","")
+        )
+        adapter.submitList(list)
+
     }
 
     companion object {

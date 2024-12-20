a9aec4f8ce8b1677199d377b12c5448c2aa6e11d
muktarkhanabilda <117581921+muktarkhanabilda@users.noreply.github.com>
2024-12-20 20:14:09 +0600

    m

--- app/src/main/java/com/example/weatherapp_final/MainViewModel.kt
+++ app/src/main/java/com/example/weatherapp_final/MainViewModel.kt
@@ -2,9 +2,10 @@ package com.example.weatherapp_final
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.weatherapp_final.adapters.WeatherModel
 
 class MainViewModel : ViewModel() {
-    val liveDataCurrent = MutableLiveData<String>()
-    val liveDataList = MutableLiveData<List<String>>()
+    val liveDataCurrent = MutableLiveData<WeatherModel>()
+    val liveDataList = MutableLiveData<List<WeatherModel>>()
 
 }
--- app/src/main/java/com/example/weatherapp_final/fragments/MainFragment.kt
+++ app/src/main/java/com/example/weatherapp_final/fragments/MainFragment.kt
@@ -1,18 +1,33 @@
 package com.example.weatherapp_final.fragments
 
+import android.Manifest
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.FragmentActivity
 import com.example.weatherapp_final.R
+import com.example.weatherapp_final.adapters.VpAdapter
 import com.example.weatherapp_final.databinding.FragmentMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class MainFragment : Fragment() {
+    private val flist = listOf(
+        HoursFragment.newInstance(),
+        DaysFragment.newInstance(),
+        //HistoryFragment.newInstance()
+    )
+    private val tlist = listOf(
+        "Hours",
+        "Days",
+        //"History"
+    )
     private lateinit var plauncher: ActivityResultLauncher<String>
     private lateinit var binding: FragmentMainBinding
 
@@ -27,6 +42,15 @@ class MainFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         checkPermission()
+        init()
+    }
+
+    private fun init() = with(binding){
+        val adapter = VpAdapter(activity as FragmentActivity, flist)
+        vp.adapter = adapter
+        TabLayoutMediator(tabLayout, vp){
+            tab, pos -> tab.text = tlist[pos]
+        }.attach()
     }
 
     private fun permissionListener(){
@@ -37,9 +61,9 @@ class MainFragment : Fragment() {
     }
 
     private fun checkPermission(){
-        if(!isPermissionGranted(android.Manifest.permission.ACCESS_FINE_LOCATION)){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
             permissionListener()
-            plauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION)
+            plauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         }
     }
 
